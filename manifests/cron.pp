b5150a57b941c416153ca780ce7f3dd179e754a3
lhinds <lhinds@redhat.com>
2017-02-22 10:03:02 +0000

    Improvements to cron.pp
    This change will install `mailx` and set the cron job to send an
    email if the `email` parameter is set. If `email` is not set, it
    will set a cron job to log to a date stamped file in `/var/log/audit`.

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,14 +1,18 @@
 class aide::cron inherits aide {
-  if $::aide::email {
-    package { 'mailx':
-      ensure => installed,
-  }
- }
-  cron { 'aide':
-    command => "${aide::command} --check ${aide::check_parameters} | ${aide::mail_path} -s \"\$HOSTNAME - Daily AIDE integrity check\" ${aide::email}",
-    user    => 'root',
-    hour    => $::aide::hour,
-    minute  => $::aide::minute,
-    require => [Package['aide'], Exec['install aide db']]
-  }
+   if $::aide::email {
+     $command = "${aide::command} --check ${aide::check_parameters} | ${aide::mail_path} -s \"\$HOSTNAME - Daily AIDE integrity check\" ${aide::email}"
+     package { 'mailx':
+         ensure => installed,
+       }
+   }
+   else {
+     $command = "${aide::command} --check ${aide::check_parameters} > /var/log/audit/aide_`date +%Y-%m-%d`.log"
+   }
+   cron { 'aide':
+      command => $command,
+      user    => 'root',
+      hour    => $::aide::hour,
+      minute  => $::aide::minute,
+      require => [Package['aide'], Exec['install aide db']]
+   }
 }
